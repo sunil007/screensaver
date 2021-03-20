1017bf677a1e9f2e8ba446b6f64b2891bd03438e
sunil007 <sunil.kothari.007@gmail.com>
2021-03-20 18:47:49 +0530

    app version

--- api/security/appVersion.php
+++ api/security/appVersion.php
@@ -1,1 +1,9 @@
-1
+<?php
+	$responseMap = array();
+	$responseMap['status'] = 'success';
+	$responseMap['statusCode'] = 200;
+	$message = array();
+	$message['version'] = 1;
+	$responseMap['response'] = $message;
+	return json_encode($responseMap);
+?>
