8717fd8197b76beef350fb1e1bd1c955c71dae46
Ronak Khatwad <60884079+ronakkhatwad@users.noreply.github.com>
2021-08-15 18:01:33 +0530

    Added Terms & Conditions

--- model/CompanyDetails.php
+++ model/CompanyDetails.php
@@ -6,11 +6,80 @@
 		public static $addressLine2 = "Company Addres line 2";
 		public static $gst = "GABC12345678ABCJ";
 		
-		public static $invoiceTerms = array("1. AMS : Annual Maintnance Changes","2. GST is 18% of AMC price");
-		public static $compantTerms = array("1. AMS : Annual Maintnance Changes","2. GST is 18% of AMC price","3. Annual Maintnance Changes Annual Maintnance Changes Annual Maintnance Changes Annual Maintnance Changes Annual Maintnance Changes Annual Maintnance Changes Annual Maintnance Changes Annual Maintnance Changes Annual Maintnance Changes Annual Maintnance Changes Annual Maintnance Changes Annual Maintnance Changes");
+		public static $invoiceTerms = array("1. AMC : Annual Maintnance Charges","2. GST is 18% of AMC price");
+		// public static $compantTerms = array("1. AMC : Annual Maintnance Charges","2. GST is 18% of AMC price","3. Annual Maintnance Changes Annual Maintnance Changes Annual Maintnance Changes Annual Maintnance Changes Annual Maintnance Changes Annual Maintnance Changes Annual Maintnance Changes Annual Maintnance Changes Annual Maintnance Changes Annual Maintnance Changes Annual Maintnance Changes Annual Maintnance Changes");
 		
 		public static $contactPhone = "+91 90-991-991-71";
 		public static $contactEmail = "support@onesecure.in";
-		
+        
+        public static $compantTerms = array("1.	 DEFINITIONS",
+        "1.1	\"Annual Maintenance Plan or Annual Maintainance Charge(AMCs)\" shall mean services package offered by OneSecure from time to time, for only mobile displays.",
+        "1.2	\"Plan Fee\" shall mean the one time fees charged by OneSecure for the Annual Maintenance Plan(s) availed of by the Customer and set out in the respective Plan Terms. The Plan Fee is applicable for the respective duration of the plan as mentioned below. The Plan Fee is inclusive of all applicable taxes.",
+        "1.3	\"Plan Terms\" shall mean the terms and conditions separately provided with the Terms herein which shall be specifically applicable in relation to each Annual Maintenance Plan(s).",
+        "1.4	\"Service Partner\" means any third party service provider affiliated with OneSecure.",
+	"1.5	\"Personal Information/Data\" shall mean and include such personal and financial information of the Customer relating to his/her data /or documents, in any medium including financial information such as bank account or credit card or debit card or other payment instrument details, identification document details including passport, PAN card details, driving license, etc.",        
+	"2.PURPOSE",
+       " 2.1	These terms and conditions (\"Terms\") shall govern the transaction between OneSecure by Sai Secure Solution Private Limited (\"OneSecure\") and the party whose name appears on the Order (\"Customer\") in relation to the Annual Maintenance Plan(s) provided by OneSecure. ",
+        
+        "2.2	These general terms and conditions define the framework and the respective obligations of the parties. Specific terms and conditions relating to the specific Plan(s) that has been availed or subscribed to by the Customer supplementing or derogating from these general terms and conditions may be agreed to in the Plan Terms in writing which shall be annexed to this Terms.",
+        "2.3	Customer acknowledges the receipt of the Terms and the Plan Terms, as applicable and agrees to be fully bound by the Terms and the relevant Plan Terms. In the event, the Customer avails of any benefit under any of the Plan Terms or lodges a claim within the term of the Annual Maintenance Plan(s), the Customer shall deemed to have accepted the Terms unconditionally.",
+       "3. CUSTOMER CONSENTS AND CONFIRMATIONS",
+        "3.1	Further, the Customer has and hereby consents to the use of the Personal Information by OneSecure for the purposes of providing the various services under the Annual Maintenance Plan(s) offered by OneSecure. OneSecure respects the privacy of the Customer and the confidentiality of Customer’s Personal Information so collected by OneSecure by itself or on its behalf and shall take all reasonable steps to protect it and maintain its confidentiality.",
+        "3.2	The Customer also hereby consents to the Personal Information being disclosed by OneSecure to any third party including any Service Partner of OneSecure who will be either providing other benefit and/or services on each of the Annual Maintenance Plan(s) for the purposes of fulfillment of the services or if required by law.",
+        "3.3	The Customer expressly and without limitation, consents to OneSecure or its service partners recording phone calls between the Customer and OneSecure on the helpline numbers set out in the relevant Plan Terms in order for OneSecure to inter alia (i) provide a record of the instructions received from the Customer and to share the same with the Service Partners, if required, (ii) allow itself or its service partners to monitor quality standards, (iii) training purposes, and (iv) meet legal and regulatory requirements.",
+       "3.4	The Customer acknowledges that OneSecure has the sole right to vary the features/benefits under the Annual Maintenance Plan(s) or the Annual Maintenance Plans or the amount or rate of the Plan Fee or part thereof, from time to time.",
+      	"3.5	The Customer acknowledges that OneSecure may engage third parties including Service Partners for the fulfillment of the services and the Customer hereby consents to OneSecure disclosing, to the extent relevant, the Customer’s Personal Information and/or details of Annual Maintenance Plan(s) availed by the Customer to inter alia (a) our affiliates Service Partners (b) to our suppliers, vendors, for the purposes of servicing the Customer.",
+        "3.6	The Customer hereby consents to OneSecure identifying any service providers and/or products that may be of some interest to the Customer.",
+        "4.	TOTAL FEES/CHARGES",
+       "4.1	OneSecure shall charge the One Time Plan Fee from the Customer for availing of the Annual Maintenance Plan(s) for the duration of the respective Plan. The Plan Fee shall be payable in advance and the Customer have to make a one-time payment of the Plan Fee for the applicable period including applicable taxes and levies.",
+        "4.2	Activation of the Annual Maintenance Plan(s) is subject to realization/ receipt of the Plan Fee.",
+        "5.	CANCELLATIONS/ RENEWAL/TERMINATION",
+        "5.1	OneSecure will cancel the Terms and/or the Plan Terms if OneSecure does not receive the Plan Fee (all inclusive) on the date it is due.",
+        "5.2	OneSecure will cancel the Terms and/or the Plan Terms if the Customer has at any time: ",
+        "a)	agreed to help any third party to try to fraudulently or dishonestly obtain money from OneSecure or the Service Partner; or ",
+        "b)	is in violation of applicable law as may be relevant to the use of the Annual Maintenance Plan(s); or ",
+        "c)	failed to meet the Terms and/or the Plan Terms, or to act in good faith, openly, honestly and in a bona fide manner towards OneSecure or the Service Partner including by providing false or inaccurate information; and ",
+        "d)	Customer fails to return to OneSecure or its authorized Service Partner(s) any goods/devices temporarily lent to him/her or money advanced on an emergency basis to him/her or tickets or hotel bookings made on behalf of the Customer and which are to be returned/reimbursed/paid as per the terms of the Plan to OneSecure or to its authorized Service Partner.",
+        "5.3	After the payment of the Plan Fee (all inclusive) and/or Activation of Annual Maintenance Plan(s) by OneSecure, the Annual Maintenance Plan(s) cannot be terminated and no refund will be available.",
+       "5.4	If the Annual Maintenance Plan(s) is Rejected due to providing incorrect IMEI Number or incorrect mobile company or incorrect mobile model or incorrect mobile images or mobile display is damaged already, Plan Fee after deducting the transaction charges shall be refunded.",
+        "5.5	If OneSecure fails to activate Annual Maintenance Plan(s) purchased by customer within provided time period, complete Plan Fee will be refunded to customer.",
+        "5.6	If the Customer has lodged a claim or availed of any benefit under any of the Plan Terms any time during the term of the plan, no refund will be available.",
+        "6.	CONFIDENTIALITY",
+        "6.1	OneSecure shall make reasonable efforts to ensure that the Personal Information of the Customer is kept confidential and not disclosed to any third party except to the extent required for fulfillment of services.",
+        "7.	OBLIGATIONS AND COVENANTS OF THE CUSTOMER",
+        "7.1	If the Customer receives a benefit as contemplated under any specific Plan Terms and it is later discovered that the claim was dishonest, fraudulent or false, OneSecure will take steps to recover from the Customer, such payment(s) made to the Customer, either by OneSecure or a third party, as the case may be.",
+        "7.2	The Customer undertakes that he/she shall strictly comply with the terms of usage contained in the Plan Terms in relation to the use of the Annual Maintenance Plan(s).",
+        "7.3	The Customer acknowledges, confirms and covenants that that the object of the Annual Maintenance Plan(s) being availed of or provided by OneSecure is not an ‘insurance product’ or insurance and that the Customer has availed of the Annual Maintenance Plan(s) in accordance with this understanding.",
+        "7.4	The Customer undertakes and covenants that he/she shall not use make use of the Annual Maintenance Plan(s) to or in the course of usage of the Annual Maintenance Plan(s), upload, display, publish, update, disseminate or transmit content or information that: ",
+        "a)	belongs to another person and to which the user does not have any right to or which is confidential; ",
+       "b)     is an impersonation of another person, grossly harmful, harassing, blasphemous defamatory, obscene, pornographic, pedophilic, libelous, invasive of another’s privacy, hateful, or racially, ethnically objectionable, disparaging, relating or encouraging money laundering or gambling, or otherwise unlawful in any manner whatever; ",
+       "c)     harm minors in any way", 
+        "d)	infringes any patent, trademark, copyright or other proprietary rights",
+        "e)	deceives or misleads the addressee about the origin of such messages or communicates any information which is grossly offensive or menacing in nature",
+       "f)	contains software viruses or any other computer code, files or programs designed to interrupt, destroy or limit the functionality of any computer resource or ",
+        "g)	Threatens the unity, integrity, defense, security or sovereignty of India or seditious, friendly relations with foreign states, or public order or causes incitement to the commission of any cognizable offence or prevents investigation of any offence or is insulting to any other nation or violates any other provision of law.",
+      "8.	LIMITATION OF LIABILITY",
+        "8.1	OneSecure shall not be liable for any incidental, consequential, exemplary, special or indirect damages (including, but not limited to, loss of profits, revenues, data and/or use). OneSecure disclaims all implied warranties of merchantability, fitness for a particular purpose, and non-infringement. OneSecure’s total liability under the Terms and/or the relevant Plan Terms shall not exceed the Plan Fee.",
+        "8.2	OneSecure shall not be liable in respect of loss or damage to Equipment under our Annual Maintenance Plan(s) relating to or caused due to the following: ",
+        "1.	Your Product has had its serial or IMEI number removed, defaced or altered",
+       "2.	Loss due to Intentional act or wilful neglect.",
+        "3.	Loss arising after 12 months from date of inception of the Protection Plan sold to the Beneficiary.",
+        "4.	Any loss due to hire or loan of the under Plan equipment to a third party or if ownership is transferred",
+        "5.	Consequential loss of any kind or description including wear & tear, manufacturing defects etc.",
+        "6.	 Loss covered by supplier, dealer or factory warranty",
+        "7.	Any loss of data or software installed in the equipment",
+        "8.	Any loss arising outside the territorial limits of India.",
+        "9.	Any loss that is cosmetic in nature and does not result in to complete stoppage of/or functioning of equipment",
+        "10. Any loss effecting to SIM card and any ancillary product etc. even if the equipment result into complete stoppage of working.",
+        "11. Any loss to mobile equipment or electronic components or accessories such as battery, charger, earphones etc. other than mobile display are not included. ",
+      "9. COVERAGE",
+        "Coverage Summary",
+        "Accidental Damage(Only Mobile Display) - Covered",
+        "Mobile Accessories - Not Covered",
+        "Claims Limit - 1 claim during 12 month service period"
+        );
 	}
 ?>
+
+
+
