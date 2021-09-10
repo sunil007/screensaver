38bdfe9a418339ae0fdf6ecb229388f41344dc3b
sunil007 <sunil.kothari.007@gmail.com>
2021-09-10 18:02:28 +0530

    bug

--- model/Securevault.php
+++ model/Securevault.php
@@ -6,8 +6,8 @@
 		public $name;
 		public $value;
 		
-		public $RAZER_PAY_ID = "RAZER_PAY_ID";
-		public $RAZER_PAY_SECRET = "RAZER_PAY_SECRET";
+		public static $RAZER_PAY_ID = "RAZER_PAY_ID";
+		public static $RAZER_PAY_SECRET = "RAZER_PAY_SECRET";
 		
 		public function populateByRow($row){
 			$this->id = $row['id'];
