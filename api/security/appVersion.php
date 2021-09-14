6201c379300706fd4c0e69dc1629f45ca7b1f8be
Ronak Khatwad <60884079+ronakkhatwad@users.noreply.github.com>
2021-09-14 18:24:04 +0530

    Version-3

--- api/security/appVersion.php
+++ api/security/appVersion.php
@@ -3,7 +3,7 @@
 	$responseMap['status'] = 'success';
 	$responseMap['statusCode'] = 200;
 	$message = array();
-	$message['version'] = 1;
+	$message['version'] = 3;
 	$message['minVersion'] = 3;
 	$message['url'] = "https://play.google.com/store/apps/details?id=com.onesecurepvt.onesecureapp";
 	$responseMap['response'] = $message;
