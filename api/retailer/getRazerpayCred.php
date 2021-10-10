dde8f09f4d8f959f3bb9ae3f4f8d70fa642a3dd0
Ronak Khatwad <60884079+ronakkhatwad@users.noreply.github.com>
2021-10-10 21:40:27 +0530

    added getRazorpayCreds

--- api/retailer/getRazerpayCred.php
+++ api/retailer/getRazerpayCred.php
@@ -1,1 +1,30 @@
+<?php
+	include_once __DIR__."/../config/timezone.php";
+	include_once __DIR__."/../config/retailer_security.php";
+	include_once __DIR__."/../../model/OTP.php";
+	include_once __DIR__."/../../model/Token.php";
+	include_once __DIR__."/../../model/Response.php";
+	include_once __DIR__."/../../model/Retailer.php";
+	include_once __DIR__."/../../model/entity.php";
+	
+	$mobile = $_POST['mobile'];
+	$token = $_POST['token'];
+	$userId = Token::getTokenUserId($token, $mobile);
 
+	$user = Retailer::getUserById($userId);
+	if($user){
+		if($user->status == 1){
+			$credMap = Securevault::getByNames(Securevault::$RAZER_PAY_ID.','.Securevault::$RAZER_PAY_SECRET);
+			$map = array();
+			$map['id'] = $credMap[Securevault::$RAZER_PAY_ID]->value;
+			$map['secret'] = $credMap[Securevault::$RAZER_PAY_SECRET]->value;
+			echo Response::getSuccessResponse($map, 200);
+		}else if($user->status == -1){
+			echo Response::getFailureResponse(null, 408);
+		}else{
+			echo Response::getFailureResponse(null, 410);
+		}
+	}else{
+		echo Response::getFailureResponse(null, 407);
+	}
+?>
