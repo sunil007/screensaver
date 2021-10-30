c8efc4d6e14bd2ecc13f3dc0869617a855c9d4ef
sunil007 <sunil.kothari.007@gmail.com>
2021-10-30 18:09:40 +0530

    new service added - add mobile model

---
+++ api/salesexecutive/addMobile.php
@@ -0,0 +1,29 @@
+<?php
+	include_once __DIR__."/../config/timezone.php";
+	include_once __DIR__."/../config/salesexecutive_security.php";
+	include_once __DIR__."/../../model/entity.php";
+
+	$mobile = $_POST['mobile'];
+	$token = $_POST['token'];
+	$userId = Token::getTokenUserId($token, $mobile);
+	$salesExecutive = SalesExecutive::getSalesExecutiveById($userId);
+	if(!SalesExecutive::isSalesExecutiveValid($salesExecutive)){
+		echo Response::getFailureResponse(null, 422);exit(0);
+	}
+
+	if(isset($_POST['company']) && isset($_POST['model']) && isset($_POST['price']) && is_numeric($_POST['price'])){
+
+		$company = $_POST['company'];	
+		$model = $_POST['model'];
+		$price = $_POST['price'];
+		$mobile = new Mobile();
+		$responseNum = $mobile->addMobile($company, $model, $price);
+		if($responseNum == 200)
+			echo Response::getSuccessResponse(null, 200);
+		else
+			echo Response::getFailureResponse(null, $responseNum);
+	}else{
+		echo Response::getFailureResponse(null, 409);
+	}
+
+?>
--- model/Mobile.php
+++ model/Mobile.php
@@ -13,13 +13,17 @@
 			return $company;
 		}
 		
-		public function getMobileModel($companyName){
-			$query = "select * from mobile where company = '".$companyName."' and status = 1";
+		public function getMobileModel($companyName, $model = null){
+			$query = "select * from mobile where company = '".$companyName."' and status = 1 ";
+			if($model != null){
+				$query .= " and model = '".$model."' ";
+			}
 			$resultSet = dbo::getResultSetForQuery($query);
 			$company=array();
 			if($resultSet != false){
 				while($row = mysqli_fetch_array($resultSet)){
 					$dataObj = array();
+					$dataObj['id'] = $row['id'];
 					$dataObj['model'] = $row['model'];
 					$dataObj['price'] = $row['price'];
 					$dataObj['policyAmount'] = Policy::calculatePolicyPrice($row['price']);
@@ -29,6 +33,22 @@
 				}
 			}
 			return $company;
+		}
+		
+		public function addMobile($com, $mod, $price){
+			$checkModel = $this->getMobileModel($com, $mod);
+			if(sizeof($checkModel) > 0){
+				/*Update Price*/
+				foreach($checkModel as $model){
+					$query = "UPDATE `mobile` SET `price` = '".$price."' WHERE `id` = ".$model['id'].";";
+					dbo::updateRecord($query);
+				}
+			}else{
+				/*Insert Model*/
+				$query = "INSERT INTO `mobile` (`id`, `company`, `model`, `price`, `status`) VALUES (NULL, '".$com."', '".$mod."', '".$price."', '1');";
+				dbo::insertRecord($query);
+			}
+			return 200;
 		}
 	}
 ?>
