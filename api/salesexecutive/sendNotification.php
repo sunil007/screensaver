951f4bcee49d665b27dedd6ae44d4afbfe14e07c
Ronak Khatwad <2019kucp1040@iiitkota.ac.in>
2021-11-15 09:35:44 +0530

    Added sendNotification

---
+++ api/salesexecutive/sendNotification.php
@@ -0,0 +1,38 @@
+<?php
+	include_once __DIR__."/../config/timezone.php";
+	include_once __DIR__."/../config/salesexecutive_security.php";
+	include_once __DIR__."/../../model/entity.php";
+	include_once __DIR__."/../../model/login.php";
+	$mobile = $_POST['mobile'];
+	$token = $_POST['token'];
+
+	$userId = Token::getTokenUserId($token, $mobile);
+	$salesExecutive = SalesExecutive::getSalesExecutiveById($userId);
+	if(!SalesExecutive::isSalesExecutiveValid($salesExecutive)){
+		echo Response::getFailureResponse(null, 422);exit(0);
+	}
+	if(isset($_POST['title']) && isset($_POST['body']) && isset($_POST['type'])){
+		$title = $_POST['title'];
+		$body = $_POST['body'];
+		$type = $_POST['type'];
+
+$response  = FireBaseNotification::SendNotificationToUser(null, $type, $title,$body);
+$arr = json_decode($response, true);
+
+echo $response;
+
+$responseNum = 200;
+if($arr["failure"] == 0)
+ $responseNum = 200;
+else
+ $responseNum = 430;
+
+		if($responseNum == 200)
+			echo Response::getSuccessResponse(null, 200);
+		else
+			echo Response::getFailureResponse(null, $responseNum);
+	}else{
+		echo Response::getFailureResponse(null, 409);
+	}
+
+?>
--- model/FireBaseNotification.php
+++ model/FireBaseNotification.php
@@ -3,18 +3,24 @@
 	class FireBaseNotification{
 		private static $key = 'AAAAQ3OxKi0:APA91bF0dUCFsTkCsmMRYNTSoiIomyYIM1bKlVLb202uI4fo2D2ic29grnbSSNE55jEDght_Wh3MrNlkYyjBJOg301mu3pCabutw5uKRBNNeSQICfAoUqkwevuJA_uVDMzX5WU54J-7F';
 		public static function SendNotificationToUser($refid, $userType, $title, $text){
-			
-			$logins = Login::getLoginByRefIdAndType($refid, $userType);
+
+			if($userType == "ALL")
+			{
+				$logins = Login::getAllLogins();
+			}else{
+				$logins = Login::getLoginByRefIdAndType($refid, $userType);
+			}
+
 			$usersKeyArray = array();
 			foreach($logins as $login){
 				array_push($usersKeyArray, $login->firebaseId);
 			}
-			
+
 			define( 'API_ACCESS_KEY', FireBaseNotification::$key );
 			{
 				//$msg = array('body' 	=> $text,'title'	=> $title, 'click_action' => 'classmatrix.scheduledetail.activity');
 				$msg = array('body' 	=> $text,'title'	=> $title);
-				
+
 				if(!isset($data)){
 					$data = array();
 				}
@@ -23,8 +29,8 @@
 				$data['BODY'] = $text;
 				$fields = array('registration_ids'=> $usersKeyArray, 'notification'	=> $msg);
 				$headers = array('Authorization: key=' . API_ACCESS_KEY,'Content-Type: application/json');
-			
-				#Send Reponse To FireBase Server	
+
+				#Send Reponse To FireBase Server
 				$ch = curl_init();
 				curl_setopt( $ch,CURLOPT_URL, 'https://fcm.googleapis.com/fcm/send' );
 				curl_setopt( $ch,CURLOPT_POST, true );
@@ -35,11 +41,11 @@
 				/*AS OF NOW STOPING FIRE BASE NOTIFICATION*/
 				$result = curl_exec($ch );
 				curl_close( $ch );
-				
+
 				#Echo Result Of FireBase Server
-				//echo $result;
+				return $result;
 			}
-			
+
 		}
 	}
 
